c680426d8b0ae154e55f5520412d37edd143c4ff
smallmonkeykey <sarinitakuoo@gmail.com>
2023-09-26 11:36:04 +0900

    Authorを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_25_014300) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_26_020400) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "author"
   end
 
 end
